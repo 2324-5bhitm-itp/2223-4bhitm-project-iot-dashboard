2c549fe9efefe07dbe1ace68b838ca95b90c4a0a
Paul Horner <paul.horner.2002@gmail.com>
2023-05-22 14:25:08 +0200

    Angular delete mqtt compnent and service

--- newDasboard/src/app/app.module.ts
+++ newDasboard/src/app/app.module.ts
@@ -4,27 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { MqttService } from 'ngx-mqtt';
-import { MQTT_CONFIG } from './mqtt.config';
-import { MqttComponentComponent } from './mqtt-component/mqtt-component.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    MqttComponentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [
-    {
-      provide: MqttService,
-      useFactory: () => {
-        return new MqttService(MQTT_CONFIG);
-      },
-    },
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
