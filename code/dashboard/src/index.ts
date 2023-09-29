cc4304e01de21ab59fad9905b8de908cb648739a
Paul Horner <paul.horner.2002@gmail.com>
2023-09-29 11:30:14 +0200

    #Iot-92 To Verify

--- code/dashboard/src/index.ts
+++ code/dashboard/src/index.ts
@@ -3,7 +3,6 @@ enableMapSet()
 
 import "./component/dashboard-component"
 import "./component/linechart-component"
-//import "./component/doughchart-component"
 //import { mock } from "./model/mock-dashboard"
 import "./mqtt/mqtt"
 import "./mqtt/mqtt-faker"
