b6123005a4c58ddbc7e79d2707f421bd67c8b493
Paul Horner <paul.horner.2002@gmail.com>
2023-11-23 10:52:14 +0100

    cleaned up dashboard-components

--- code/dashboard/src/component/dashboard-component.ts
+++ code/dashboard/src/component/dashboard-component.ts
@@ -1,112 +1,110 @@
-import {html, render} from "lit-html"
-import {DashboardModel, Sensor, store} from "../model"
-import {filter, map} from "rxjs"
+import { html, render } from "lit-html"
+import { DashboardModel, Sensor, store } from "../model"
+import { filter, map } from "rxjs"
 import _ from "lodash"
-import {styles} from "../styles/styles"
-import {mqttConfig} from "../mqtt"
+import { styles } from "../styles/styles"
+import { mqttConfig } from "../mqtt"
 import "./linechart-component"
 
 import "./connection-icon"
-import {unitOfSensorName} from "../model/dashboard-model"
-import {LineChartComponent} from "./linechart-component";
+import { unitOfSensorName } from "../model/dashboard-model"
+import { LineChartComponent } from "./linechart-component";
 
 interface BoxViewModel {
-    name: string
-    sensors: Sensor[]
+  name: string
+  sensors: Sensor[]
 }
 
 interface AppComponentViewModel {
-    boxes: BoxViewModel[]
+  boxes: BoxViewModel[]
 }
 
 class DashboardComponent extends HTMLElement {
-  static id = 0;
-    constructor() {
-        super()
-        this.attachShadow({mode: "open"})
-    }
+  constructor() {
+    super()
+    this.attachShadow({ mode: "open" })
+  }
 
-    connectedCallback() {
-        store
-            .pipe(
-                filter(dashboard => !!dashboard),
-                filter(model => !!model.boxes),
-                map(toViewModel)
-            )
-            .subscribe(vm => this.render(vm))
+  connectedCallback() {
+    store
+      .pipe(
+        filter(dashboard => !!dashboard),
+        filter(model => !!model.boxes),
+        map(toViewModel)
+      )
+      .subscribe(vm => this.render(vm))
 
-    }
+  }
 
-    render(vm: AppComponentViewModel) {
-        render(template(vm), this.shadowRoot)
-    }
+  render(vm: AppComponentViewModel) {
+    render(template(vm), this.shadowRoot)
+  }
 }
 
 customElements.define("dashboard-component", DashboardComponent)
 
 function toViewModel(model: DashboardModel) {
-    const vm: AppComponentViewModel = {
-        boxes: []
+  const vm: AppComponentViewModel = {
+    boxes: []
+  }
+  model.boxes.forEach((box, name) => {
+    const boxModel: BoxViewModel = {
+      name,
+      sensors: []
     }
-    model.boxes.forEach((box, name) => {
-        const boxModel: BoxViewModel = {
-            name,
-            sensors: []
-        }
-        box.sensors.forEach((sensor, sensorName) => {
-            boxModel.sensors.push(_.clone(sensor))
-        })
-        boxModel.sensors.sort((l, r) => l.name.toLowerCase().localeCompare(r.name.toLowerCase()))
-        vm.boxes.push(boxModel)
+    box.sensors.forEach((sensor, sensorName) => {
+      boxModel.sensors.push(_.clone(sensor))
     })
-    vm.boxes.sort((l, r) => l.name.toLowerCase().localeCompare(r.name.toLowerCase()))
-    return vm
+    boxModel.sensors.sort((l, r) => l.name.toLowerCase().localeCompare(r.name.toLowerCase()))
+    vm.boxes.push(boxModel)
+  })
+  vm.boxes.sort((l, r) => l.name.toLowerCase().localeCompare(r.name.toLowerCase()))
+  return vm
 }
 
+
 function boxTemplate(box: BoxViewModel) {
-    var splitMqttName = box.name.split("/");
-  
-    const rows = box.sensors.map((sensor) => {
-      function getColorSquare(valueString) {
-        if (sensor.name !== "neopixel") return "";
-  
-        const digitArray = Array.from(valueString, (digit) => +digit * 255);
-        const colorStyle = `rgb(${digitArray[0]}, ${digitArray[1]}, ${digitArray[2]})`;
-  
-        return html`
+  var splitMqttName = box.name.split("/");
+
+  const rows = box.sensors.map((sensor) => {
+    function getColorSquare(valueString) {
+      if (sensor.name !== "neopixel") return "";
+
+      const digitArray = Array.from(valueString, (digit) => +digit * 255);
+      const colorStyle = `rgb(${digitArray[0]}, ${digitArray[1]}, ${digitArray[2]})`;
+
+      return html`
           <span style="font-size: xxx-large; color: ${colorStyle};">&#9632;</span>`;
+    }
+
+    const unit = unitOfSensorName[sensor.name];
+    const colorSquare = getColorSquare(sensor.value.toString());
+
+
+    if (sensor.name === "temperature") {
+
+      const sensorName = sensor.name
+      const chartElement = document.querySelector(`line-chart-component[sensorName="${sensorName}"]`)
+
+      console.log("This is the chart element: " + chartElement)
+
+      if (chartElement) {
+        console.log("This is the chart element: " + chartElement)
+        //chartElement.updateChartData(sensor.lastValueReceivedAt, sensor.value);
       }
-  
-      const unit = unitOfSensorName[sensor.name];
-      const colorSquare = getColorSquare(sensor.value.toString());
 
-
-      if (sensor.name === "temperature") {
-        DashboardComponent.id++;
-          const selector = `line-chart-component[sensorName="${sensor.name}"]`;
-          const chartElement = document.querySelector(selector) as LineChartComponent;
-          /*const selector = `line-chart-component[#1]`;
-          const chartElement = document.querySelector(selector) as LineChartComponent;*/
-          
-          console.log("This is the chart element: " + chartElement + "id:" + DashboardComponent.id)
-          
-          if (chartElement) {
-              chartElement.updateChartData(sensor.lastValueReceivedAt, sensor.value);
-          }
-
-        return html`
+      return html`
           <tr>
             <td>Temperature</td>
             <td class="w3-right">
-              <!--<line-chart-component .sensorName="${sensor.name}"></line-chart-component>-->
-              <line-chart-component id="${DashboardComponent.id}"></line-chart-component>
+              <line-chart-component .sensorName="${sensor.name}"></line-chart-component>
               ${Number(sensor.value.toFixed(2))} ${unit}
             </td>
           </tr>`;
-      }
+    }
 
-  
-      return html`
+
+    return html`
         <tr>
           <td>${sensor.name}</td>
           <td class="w3-right">
@@ -115,9 +113,9 @@ function boxTemplate(box: BoxViewModel) {
             ${colorSquare}
           </td>
         </tr>`;
-    });
-  
-    return html`
+  });
+
+  return html`
       <div class="w3-container w3-sans-serif">
         <div class="w3-panel">
           <div class="room">
@@ -139,12 +137,12 @@ function boxTemplate(box: BoxViewModel) {
         </div>
       </div>
     `;
-  }
+}
 
 
 function template(vm: AppComponentViewModel) {
-    const boxes = vm.boxes.map(boxTemplate)
-    return html`
+  const boxes = vm.boxes.map(boxTemplate)
+  return html`
         ${styles}
         <style>
             .box-container {
